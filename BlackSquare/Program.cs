d51a6d066ea0ff01e42fc5993c981f6fd3cdb4a5
unknown <islammohamedahmed49@gmail.com>
2024-04-14 14:29:36 +0200

    bs

---
+++ BlackSquare/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+int[] arr1 = new int[4];
+int res = 0;
+
+
+string str1 = ReadLine();
+string str2 = ReadLine();
+var arr2 = str1.Split(" ");
+for(int i = 0; i< 4; i++)
+{
+    arr1[i] = ToInt32(arr2[i]);
+}
+for(int j = 0; j < str2.Length; j++)
+{
+    int x = str2[j]-'0';
+    res += arr1[x - 1];
+}
+
+
+WriteLine(res);
+
+
+
+
+
+
+
